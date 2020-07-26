d8ae3dfca58327d106b68aacd4897b0219bddf1f
devillryu <sdryu1@hotmail.com>
2020-07-26 16:42:48 +0700

    AddNovice&Swordman

---
+++ Novice.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Novice : Character
+{
+    void Start()
+    {
+        Atk = 3;
+        print(Status);
+        print(Atk);
+        print(Level);
+    }
+}
---
+++ Swordman.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swordman : Character
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Atk = 10;
+    }
+    public void SuperAttack()
+    {
+        //Enemy Hp -= (Atk + BonusAtk)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
